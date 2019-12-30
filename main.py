715808f73e737dd97b8e447d47b205e65347e746
YTMartian <1773741250@qq.com>
2019-12-30 22:14:08 +0800

    window move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QMainWindow, QGraphicsDropShadowEffect
-from PyQt5.QtCore import pyqtSignal
-from PyQt5.QtGui import QIcon, QColor
+from PyQt5.QtCore import pyqtSignal, QPoint
+from PyQt5.QtGui import QIcon, QColor, QCursor, QEnterEvent
 from PyQt5 import QtWidgets
 from PyQt5 import QtCore
 from PyQt5 import QtGui
@@ -20,8 +20,10 @@ class MainWindow(QMainWindow):
         self.background.setBrush(self.backgroundRole(), QtGui.QBrush(QtGui.QPixmap(':/background')))
         self.setPalette(self.background)
         self.setWindowFlag(QtCore.Qt.FramelessWindowHint)  # hide the frame.
-        self.setMouseTracking(True)
         self.is_maximized = False
+        self.setMouseTracking(True)
+        self.move_window_flag = False
+        self.move_position = None
 
         self.main_widget = QtWidgets.QWidget()  # main window.
         self.main_layout = QtWidgets.QGridLayout()  # create main window layout.
@@ -36,6 +38,7 @@ class MainWindow(QMainWindow):
         self.top_layout.addStretch()
         self.top_layout.setAlignment(QtCore.Qt.AlignTop)
         self.top_widget.setLayout(self.top_layout)  # set top part layout.
+        # self.top_widget.installEventFilter(self)  # bind event filter.
 
         self.left_widget = QtWidgets.QWidget()  # create left part.
         self.left_widget.setObjectName('left_widget')
@@ -43,60 +46,68 @@ class MainWindow(QMainWindow):
         self.left_layout.setContentsMargins(10, 10, 10, 10)
         self.left_widget.setLayout(self.left_layout)  # set left part layout.
         self.left_widget_shadow = QGraphicsDropShadowEffect()  # set shadow.
-        self.left_widget_shadow.setBlurRadius(10)
-        self.left_widget_shadow.setColor(QColor(111, 0, 0, 500))
-        self.left_widget_shadow.setOffset(5, 5)
+        self.left_widget_shadow.setBlurRadius(50)
+        self.left_widget_shadow.setColor(QColor(0, 0, 0, 70))
+        self.left_widget_shadow.setOffset(10, 0)
         self.left_widget.setGraphicsEffect(self.left_widget_shadow)
         self.left_widget.setStyleSheet('''
             QWidget#left_widget{
-                background-color: transparent;
+                background-color: rgba(131,148,155,0.25);
             }
         ''')
 
         self.top_close = QtWidgets.QPushButton()  # close window button.
-        self.top_close.setFixedSize(30, 30)
-        self.top_close.setIcon(QIcon(":/close"))
+        self.top_close.setFixedSize(25, 25)
+        # self.top_close.setIcon(QIcon(":/close"))
         self.top_close.setToolTip('关闭')
+        self.top_close.setCursor(QCursor(QtCore.Qt.PointingHandCursor))  # set cursor when hover.
         self.top_close.setStyleSheet('''
             QPushButton{
                 background-color:transparent;
                 border-radius:5px;
                 color:white;
                 padding-bottom:2px;
+                background-image:url(:/minimize);
             }
             QPushButton:hover{
-                background:red;
+                background-image:url(:/close);
+                background-repeat:no-repeat center
             }
         ''')
         self.top_close.clicked.connect(self.top_close_clicked)
         self.top_maximize = QtWidgets.QPushButton()  # enlarge window button.
-        self.top_maximize.setIcon(QIcon(":/maximize"))
-        self.top_maximize.setFixedSize(30, 30)
+        # self.top_maximize.setIcon(QIcon(":/maximize"))
+        self.top_maximize.setFixedSize(25, 25)
         self.top_maximize.setToolTip('最大化')
+        self.top_maximize.setCursor(QCursor(QtCore.Qt.PointingHandCursor))  # set cursor when hover.
         self.top_maximize.setStyleSheet('''
             QPushButton{
-                background-color:transparent;
+                background-image:url(:/close);
                 border-radius:5px;
                 color:white;
                 padding-bottom:2px;
             }
             QPushButton:hover{
-                background:#5cce5a;
+                background-image:url(:/maximize);
+                background-repeat:no-repeat center
             }
         ''')
         self.top_maximize.clicked.connect(self.top_maximize_clicked)
         self.top_minimize = QtWidgets.QPushButton()  # minimize window button.
-        self.top_minimize.setFixedSize(30, 30)
-        self.top_minimize.setIcon(QIcon(":/minimize"))
+        self.top_minimize.setFixedSize(25, 25)
+        # self.top_minimize.setIcon(QIcon(":/minimize"))
         self.top_minimize.setToolTip('最小化')
+        self.top_minimize.setCursor(QCursor(QtCore.Qt.PointingHandCursor))  # set cursor when hover.
         self.top_minimize.setStyleSheet('''
             QPushButton{
                 background-color:transparent;
                 border-radius:5px;
                 color:white;
+                background-image:url(:/maximize);
             }
             QPushButton:hover{
-                background:#efef00;
+                background-image:url(:/minimize);
+                background-repeat:no-repeat center
             }
         ''')
         self.top_minimize.clicked.connect(self.showMinimized)
@@ -122,6 +133,30 @@ class MainWindow(QMainWindow):
             self.showMaximized()
             self.top_maximize.setToolTip('还原')
         self.is_maximized = not self.is_maximized
+
+    # def eventFilter(self, obj, event):
+    #     print(event.type())
+    #     if event.type() == QtCore.QEvent.HoverMove:
+    #         if obj==self.top_widget:
+    #             print('fffff')
+    #         return True
+    #     return False
+
+    def mousePressEvent(self, event):
+        top_widget_height = self.top_widget.size().height()
+        now_mouse_y = event.globalPos().y() - self.pos().y()
+        if event.button() == QtCore.Qt.LeftButton and now_mouse_y < top_widget_height:
+            self.move_window_flag = True
+            self.move_position = event.globalPos() - self.pos()  # get mouse position relative to window.
+            event.accept()  # it means the event is handled.
+
+    def mouseMoveEvent(self, QMouseEvent):
+        if QtCore.Qt.LeftButton and self.move_window_flag:
+            self.move(QMouseEvent.globalPos() - self.move_position)
+            QMouseEvent.accept()
+
+    def mouseReleaseEvent(self, QMouseEvent):
+        self.move_window_flag = False
 
 
 def main():
