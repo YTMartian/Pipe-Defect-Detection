473af48e7902ce3179d8096dfc8af1df48bda6a0
YTMartian <1773741250@qq.com>
2019-12-30 12:57:04 +0800

    frameless window

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from PyQt5.QtWidgets import QDesktopWidget
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QGraphicsDropShadowEffect
 from PyQt5.QtCore import pyqtSignal
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QIcon, QColor
 from PyQt5 import QtWidgets
 from PyQt5 import QtCore
+from PyQt5 import QtGui
 from resources import *
 import time
 import sys
@@ -14,15 +14,19 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         self.setWindowTitle('Pipe Defect Detection')
-        self.setWindowIcon(QIcon(":/app"))
+        self.setWindowIcon(QIcon(':/app'))
         self.setMinimumSize(1280, 720)
-        self.setStyleSheet('''background-color:white''')
+        self.background = QtGui.QPalette()
+        self.background.setBrush(self.backgroundRole(), QtGui.QBrush(QtGui.QPixmap(':/background')))
+        self.setPalette(self.background)
         self.setWindowFlag(QtCore.Qt.FramelessWindowHint)  # hide the frame.
+        self.setMouseTracking(True)
         self.is_maximized = False
 
         self.main_widget = QtWidgets.QWidget()  # main window.
         self.main_layout = QtWidgets.QGridLayout()  # create main window layout.
         self.main_layout.setSpacing(0)
+        self.main_layout.setContentsMargins(0, 0, 0, 0)  # the margin between layout and main window.
         self.main_widget.setLayout(self.main_layout)  # set main window layout.
 
         self.top_widget = QtWidgets.QWidget()  # create top part widget.
@@ -32,30 +36,30 @@ class MainWindow(QMainWindow):
         self.top_layout.addStretch()
         self.top_layout.setAlignment(QtCore.Qt.AlignTop)
         self.top_widget.setLayout(self.top_layout)  # set top part layout.
-        self.top_widget.setStyleSheet('''
-            QWidget#top_widget{
-                color:#232C51;
-                background:#dddddd;
-            }
-        ''')
 
         self.left_widget = QtWidgets.QWidget()  # create left part.
         self.left_widget.setObjectName('left_widget')
         self.left_layout = QtWidgets.QGridLayout()  # create left part layout.
+        self.left_layout.setContentsMargins(10, 10, 10, 10)
         self.left_widget.setLayout(self.left_layout)  # set left part layout.
+        self.left_widget_shadow = QGraphicsDropShadowEffect()  # set shadow.
+        self.left_widget_shadow.setBlurRadius(10)
+        self.left_widget_shadow.setColor(QColor(111, 0, 0, 500))
+        self.left_widget_shadow.setOffset(5, 5)
+        self.left_widget.setGraphicsEffect(self.left_widget_shadow)
         self.left_widget.setStyleSheet('''
             QWidget#left_widget{
-                color:#232C51;
-                background:#d55ddd;
+                background-color: transparent;
             }
         ''')
 
         self.top_close = QtWidgets.QPushButton()  # close window button.
         self.top_close.setFixedSize(30, 30)
         self.top_close.setIcon(QIcon(":/close"))
+        self.top_close.setToolTip('关闭')
         self.top_close.setStyleSheet('''
             QPushButton{
-                background:#F76677;
+                background-color:transparent;
                 border-radius:5px;
                 color:white;
                 padding-bottom:2px;
@@ -68,9 +72,10 @@ class MainWindow(QMainWindow):
         self.top_maximize = QtWidgets.QPushButton()  # enlarge window button.
         self.top_maximize.setIcon(QIcon(":/maximize"))
         self.top_maximize.setFixedSize(30, 30)
+        self.top_maximize.setToolTip('最大化')
         self.top_maximize.setStyleSheet('''
             QPushButton{
-                background:#6DDF6D;
+                background-color:transparent;
                 border-radius:5px;
                 color:white;
                 padding-bottom:2px;
@@ -83,9 +88,10 @@ class MainWindow(QMainWindow):
         self.top_minimize = QtWidgets.QPushButton()  # minimize window button.
         self.top_minimize.setFixedSize(30, 30)
         self.top_minimize.setIcon(QIcon(":/minimize"))
+        self.top_minimize.setToolTip('最小化')
         self.top_minimize.setStyleSheet('''
             QPushButton{
-                background:#ffef00;
+                background-color:transparent;
                 border-radius:5px;
                 color:white;
             }
@@ -95,7 +101,7 @@ class MainWindow(QMainWindow):
         ''')
         self.top_minimize.clicked.connect(self.showMinimized)
 
-        self.main_layout.addWidget(self.top_widget, 0, 0, 1, 10)  # start from (x1,y1) and occupy (x2,y2)
+        self.main_layout.addWidget(self.top_widget, 1, 0, 1, 15)  # start from (x1,y1) and occupy (x2,y2)
         self.main_layout.addWidget(self.left_widget, 1, 0, 11, 2)
         self.setCentralWidget(self.main_widget)  # set main window's main widget.
 
@@ -105,14 +111,16 @@ class MainWindow(QMainWindow):
 
     @staticmethod
     def top_close_clicked(self):
-        time.sleep(0.1)
+        time.sleep(0.1)  # so...
         exit()
 
     def top_maximize_clicked(self):
         if self.is_maximized:
             self.showNormal()
+            self.top_maximize.setToolTip('最大化')
         else:
             self.showMaximized()
+            self.top_maximize.setToolTip('还原')
         self.is_maximized = not self.is_maximized
 
 
