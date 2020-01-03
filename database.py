94f1732882c44338c8b9a4fec36695a81eba64bc
YTMartian <1773741250@qq.com>
2020-01-03 22:40:37 +0800

    show table

--- database.py
+++ database.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import pymysql
 
 
@@ -23,13 +25,34 @@ class Database:
             print(e)
 
     def get_project_detailed(self):
+        def get_name(table, id):
+            cursor = self.conn.cursor()
+            cursor.execute('select * from {0} where {0}_id = {1}'.format(table, id))
+            self.conn.commit()
+            data = cursor.fetchall()
+            return data[0][1]
+
         cursor = self.conn.cursor()
         cursor.execute('select * from project')
         self.conn.commit()
         data = cursor.fetchall()
         cursor.close()
-        print(data)
-        return []
+        res = []
+        for i in data:
+            temp = [i[j] for j in range(1, 12)]
+            temp[4] = str(temp[4])  # convert datetime.date to string.
+            detection_id = i[12]
+            move_id = i[13]
+            plugging_id = i[14]
+            drainage_id = i[15]
+            dredging_id = i[16]
+            temp.append(get_name('detection', detection_id))
+            temp.append(get_name('move', move_id))
+            temp.append(get_name('plugging', plugging_id))
+            temp.append(get_name('drainage', drainage_id))
+            temp.append(get_name('dredging', dredging_id))
+            res.append(temp.copy())
+        return res
 
     def get_project_statistic(self):
         pass
